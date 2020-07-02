9d69d77290b7d7167fa1ee57c234511df1878953
ltz0302 <45015812+ltz0302@users.noreply.github.com>
2020-07-02 16:44:14 +0800

    controller and test

---
+++ coupon-service/coupon-template/target/classes/sql/create_db.sql
@@ -0,0 +1,8 @@
+-- 登录 MySQL 服务器
+mysql -hlocalhost -uroot -ppassword
+
+-- 创建数据库 coupon
+CREATE DATABASE IF NOT EXISTS coupon;
+
+-- 登录 MySQL 服务器, 并进入到 imooc_coupon_data 数据库中
+mysql -hlocalhost -uroot -ppassword -Dcoupon
