10bfcea43db9aa3f604625c448f6932b7561abba
ltz0302 <45015812+ltz0302@users.noreply.github.com>
2020-07-06 10:44:43 +0800

    distribution interfce and vo

---
+++ coupon-service/coupon-distribution/src/main/java/com/ltz/coupon/dao/CouponDao.java
@@ -0,0 +1,20 @@
+package com.ltz.coupon.dao;
+
+import com.ltz.coupon.constant.CouponStatus;
+import com.ltz.coupon.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+/**
+ * <h1>Coupon Dao 接口定义</h1>
+ */
+public interface CouponDao extends JpaRepository<Coupon, Integer> {
+
+    /**
+     * <h2>根据 userId + 状态寻找优惠券记录</h2>
+     * where userId = ... and status = ...
+     * */
+    List<Coupon> findAllByUserIdAndStatus(Long userId, CouponStatus status);
+}
---
+++ coupon-service/coupon-distribution/src/main/java/com/ltz/coupon/service/IUserService.java
@@ -0,0 +1,49 @@
+package com.ltz.coupon.service;
+
+
+import com.ltz.coupon.entity.Coupon;
+import com.ltz.coupon.exception.CouponException;
+import com.ltz.coupon.vo.AcquireTemplateRequest;
+import com.ltz.coupon.vo.CouponTemplateSDK;
+import com.ltz.coupon.vo.SettlementInfo;
+import io.netty.channel.pool.FixedChannelPool;
+
+import java.util.List;
+
+/**
+ * <h1>用户服务相关的接口定义</h1>
+ * 1. 用户三类状态优惠券信息展示服务
+ * 2. 查看用户当前可以领取的优惠券模板 - coupon-template 微服务配合实现
+ * 3. 用户领取优惠券服务
+ * 4. 用户消费优惠券服务 - coupon-settlement 微服务配合实现
+ */
+public interface IUserService {
+    /**
+     * <h2>根据用户 id 和状态查询优惠券记录</h2>
+     * @param userId 用户 id
+     * @param status 优惠券状态
+     * @return {@link Coupon}s
+     * */
+    List<Coupon> findCouponByStatus(Long userId, Integer status) throws CouponException;
+
+    /**
+     * <h2>根据用户 id 查找当前可以领取的优惠券模板</h2>
+     * @param userId 用户 id
+     * @return {@link CouponTemplateSDK}s
+     * */
+    List<CouponTemplateSDK> findAvailableTemplate(Long userId) throws CouponException;
+
+    /**
+     * <h2>用户领取优惠券</h2>
+     * @param request {@link AcquireTemplateRequest}
+     * @return {@link Coupon}
+     * */
+    Coupon acquireTemplate(AcquireTemplateRequest request) throws CouponException;
+
+    /**
+     * <h2>结算(核销)优惠券</h2>
+     * @param info {@link SettlementInfo}
+     * @return {@link SettlementInfo}
+     * */
+    SettlementInfo settlement(SettlementInfo info) throws CouponException;
+}
